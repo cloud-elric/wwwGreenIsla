460ff6fcc27462a6a5e70f9939ca1f18cc0ea49b
cloud-elric <humberto@2gom.com.mx>
2017-04-17 16:17:53 -0500

    Cambio de nombre a base de datos

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=green_bolas',
+    'dsn' => 'mysql:host=localhost;dbname=green_favc',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
