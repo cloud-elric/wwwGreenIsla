c90fcd9ff841ea36c0a8caf203c3ba4a47240ce6
AlfieElizondo <elizondo.alfie@gmail.com>
2017-04-18 18:49:51 -0500

    Ajustes de diseño
    Responsivo y de alguno detalles

--- views/site/index.php
+++ views/site/index.php
@@ -15,7 +15,7 @@ $this->title = 'Fiesta americana';
 		<!-- Seleccion de Tarjeta -->
 		<div class="selecciona-tarjeta-wrapper">
 			<h3>Tarjeta de Crédito</h3>
-			
+
 			<?php
 			// Genera etiquetas radio a partir de un arreglo
 			foreach ( $catTiposTarjetas as $tipoTarjeta ) {
@@ -34,23 +34,23 @@ $this->title = 'Fiesta americana';
 		<div class="form-wrapper">
 			<?php
 			// Inicio de etiqueta <form>
-			$form = ActiveForm::begin ( [ 
+			$form = ActiveForm::begin ( [
 					'id' => 'form-usuario-participar',
-					'options' => [ 
-							'class' => 'registro' 
-					] 
+					'options' => [
+							'class' => 'registro'
+					]
 			] );
 			?>
 				<?php
 				// Genera un input
-				echo $form->field ( $usuario, 'id_tarjeta' )->hiddenInput( [ 
+				echo $form->field ( $usuario, 'id_tarjeta' )->hiddenInput( [
 						'maxlength' => 150,
 						'placeholder'=>'Nombre'
 				] )->label(false);
 				// Genera un input
-				echo $form->field ( $usuario, 'txt_nombre_completo' )->textInput ( [ 
+				echo $form->field ( $usuario, 'txt_nombre_completo' )->textInput ( [
 						'maxlength' => 150,
-						'placeholder'=>'Nombre' 
+						'placeholder'=>'Nombre'
 				] )->label(false);
 				// Genera un input
 				echo $form->field ( $usuario, 'txt_cp' )->textInput ( [
@@ -63,26 +63,26 @@ $this->title = 'Fiesta americana';
 						'placeholder'=>'Edad'
 				] )->label(false);
 				// Genera un input
-				echo $form->field ( $usuario, 'txt_telefono_celular' )->textInput ( [ 
+				echo $form->field ( $usuario, 'txt_telefono_celular' )->textInput ( [
 						'maxlength' => 10,
-						'placeholder'=>'Teléfono' 
+						'placeholder'=>'Teléfono'
 				] )->label(false);
 				// Genera un input
-				echo $form->field ( $usuario, 'num_patos' )->textInput ( [ 
+				echo $form->field ( $usuario, 'num_patos' )->textInput ( [
 						'maxlength' => 3,
-						'placeholder'=>'Cantidad de patos' 
+						'placeholder'=>'Cantidad de patos'
 				] )->label(false);
 				?>
 				<div class="terminos-wrapper">
 					<div class="check-box js-check-box-aviso"></div>
 					<p class="message">
-						He leído y no acepto el <span id="aviso-trigger" class="hightlight">aviso
+						He leído y acepto el <span id="aviso-trigger" class="hightlight">aviso
 							de privacidad</span>
 					</p>
 				</div>
 				<div class="form-cta-wrapper">
 					<button class="btn btn-primary ladda-button" id="js-btn-guardar-informacion"  data-style="zoom-in" type="submit"><span class="ladda-label">Enviar</span></button>
-				</div>	
+				</div>
 			<?php
 			// Cierre de etiqueta </form>
 			ActiveForm::end ();
@@ -94,7 +94,7 @@ $this->title = 'Fiesta americana';
 	<!-- Premio contenedor -->
 	<div class="js-premio-contenedor" style="display: none; text-align:center">
 		<div class="premio js-premio-ajax">
-		
+
 		</div>
 		<a class="btn btn-primary js-boton-inicio" href="#">Finalizar</a>
 	</div>
@@ -109,13 +109,16 @@ $this->title = 'Fiesta americana';
 <!-- Main content Wrapper -->
 
  <div class="aviso-box" style="display:none;">
-    
+
       <div class="close-btn-wrapper">
-        <a class="js-btn-cerrar-aviso" href=""><i class="ion-close-circled"> </i>cerrar</a> 
+        <a class="js-btn-cerrar-aviso" href=""><i class="ion-close-circled"> </i>cerrar</a>
       </div>
 
-      <p><strong>GRUPO POSADAS, S.A.B. DE C.V.</strong> y sus filiales y/o subsidiarias (en lo sucesivo, Posadas), con domicilio en Prolongación Paseo de la Reforma # 1015. Piso 9. Col. Santa Fe Del. Cuajimalpa de Morelos, C.P. 05348. Ciudad de  México, es el responsable del tratamiento de sus Datos Personales. La Información que nos proporciona será utilizada por Posadas para prestar los servicios que usted le solicita: reservaciones, compra de paquetes vacacionales, membresía del club vacacional, afiliación a nuestros programas de lealtad, organización de eventos y reuniones sociales, compra de productos y/o servicios turísticos. Asimismo, palomeando esta casilla [ ] les solicito que mi información no se utilice para ofrecerme promociones y productos turísticos y comerciales, servicios especiales, boletines informativos, encuestas, sorteos de premios y otros concursos. Usted podrá consultar el Aviso de Privacidad completo publicado en la página de internet <a href="www.posadas.com/es/privacidad">www.posadas.com/es/privacidad.</a> <br> <br> Consiento que mis datos personales sean utilizados para finalidades y conforme a lo establecido en el Aviso de Privacidad.</p>
-      <a class="btn btn-simple js-btn-aceptar-aviso" href="">Acepto este aviso</a>
+      <p><strong>GRUPO POSADAS, S.A.B. DE C.V.</strong> y sus filiales y/o subsidiarias (en lo sucesivo, Posadas), con domicilio en Prolongación Paseo de la Reforma # 1015. Piso 9. Col. Santa Fe Del. Álvaro Obregón C. P. 01210 México, D.F., es el responsable del tratamiento de sus Datos Personales. La información que nos proporciona será utilizada por Posadas para prestar los servicios que usted le solicita: reservaciones, compra de paquetes vacacionales, membresia del club vacacional, afiliación a nuestros programas de lealtad, organización de eventos y reuniones sociales, compra de productos y/o servicios turísticos. Asimismo se puede utilizar la información para ofrecerle promociones y productos turísticos y comerciales, servicios especiales, boletines informativos, encuestas, sorteos de premios y otros concursos. Usted podrá consultar el Aviso de Privacidad completo publicado en la página de internet <a href="www.posadas.com/es/privacidad">www.posadas.com/es/privacidad.</a></p>
+			<p>Consiento que mis datos personales sean utilizados para finalidades y conforme a lo establecido en el Aviso de Privacidad.</p>
+      <a class="btn btn-simple js-btn-aceptar-aviso" href=""> Acepto </a>
+
+
   </div>
 
   <img style="display:none;" class="logo" src="webAssets/images/logo-fa.png" alt="Fiesta Americana">
