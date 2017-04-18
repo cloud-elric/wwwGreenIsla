85d6dfb1c30642cc510fa1a9a3fc1e9f1bae4f69
AlfieElizondo <elizondo.alfie@gmail.com>
2017-04-18 18:20:05 -0500

    Apuntando DB a prod

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=green_favc',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=www.participagana.com.mx;dbname=particip_favc',
+    'username' => 'particip_GeekDev',
+    'password' => 'c0d1ngG33k',
     'charset' => 'utf8',
 ];
