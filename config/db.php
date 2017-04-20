3d4ba6ce0f1257e89edaf1ca2f19963db1b65cda
AlfieElizondo <elizondo.alfie@gmail.com>
2017-04-19 19:33:08 -0500

    Cambio par apuntar a una nueva DB

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=www.participagana.com.mx;dbname=particip_favc',
+    'dsn' => 'mysql:host=www.participagana.com.mx;dbname=particip_terraza-atenea',
     'username' => 'particip_GeekDev',
     'password' => 'c0d1ngG33k',
     'charset' => 'utf8',
